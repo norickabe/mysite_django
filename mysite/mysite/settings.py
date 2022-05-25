6875c4c786f92636138d87c01fdeaa97efdb6ac8
norickabe <b1749777@planet.kanazawa-it.ac.jp>
2022-05-25 13:06:48 +0000

    fix basedir

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -15,7 +15,7 @@ import os
 import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 env = environ.Env()
 env.read_env(os.path.join(BASE_DIR, '.env'))
